e9c18f4c82b55959fdb8ea936536876bbf9f5216
Takahiro55555 <tkhr.pub@gmail.com>
2020-02-05 02:41:29 +0900

    Update: https環境でないと動作しない可能性があるためとりあえずcommit

--- js/motion.js
+++ js/motion.js
@@ -1,69 +1,85 @@
 //ジャイロセンサー確認
-let isGyro = false;
+var isGyro = false;
 if ((window.DeviceOrientationEvent) && ('ontouchstart' in window)) {
     isGyro = true;
+    appendMessage("[Log] ジャイロセンサーを搭載しています");
 }
 
 //PCなど非ジャイロ
 if (!isGyro) {
-    gyroIsNotAllowed();
-
+    // ここに何か関数
+    appendMessage("[Log] ジャイロセンサーを搭載していません");
     //一応ジャイロ持ちデバイス
 } else {
     //ジャイロ動作確認
-    let resGyro = false;
+    var resGyro = false;
     window.addEventListener("deviceorientation", doGyro, false);
     function doGyro() {
         resGyro = true;
         window.removeEventListener("deviceorientation", doGyro, false);
+        gyroIsAllowed();
     }
 
     //数秒後に判定
     setTimeout(function () {
         //ジャイロが動いた
         if (resGyro) {
-            gyroIsNotAllowed();
+            // ここに何か関数
+            gyroIsAllowed();
 
             //ジャイロ持ってるくせに動かなかった
         } else {
+            gyroIsNotAllowed();
             //iOS13+方式ならクリックイベントを要求
             if (typeof DeviceOrientationEvent.requestPermission === "function") {
                 //ユーザアクションを得るための要素を表示
-                cv._start.show();
-                cv._start.on("click", function () {
-                    cv._start.hide();
+                appendMessage("[Log] iOS13+方式です");
+                const body = document.getElementById("title");
+                body.onclick = function () {
+                    appendMessage("[Log]クリックされました");
                     DeviceOrientationEvent.requestPermission().then(res => {
+                        appendMessage("[Log] 許可を確認ダイアログが出現するはずです...");
                         //「動作と方向」が許可された
                         if (res === "granted") {
-                            gyroIsNotAllowed();
+                            // ここに何か関数
+                            gyroIsAllowed();
+                            appendMessage("[Log] 動作と方向が許可されました");
                             //「動作と方向」が許可されなかった
                         } else {
                             isGyro = false;
+                            // ここに何か関数
                             gyroIsNotAllowed();
+                            appendMessage("[Log] 動作と方向が許可されませんでした");
                         }
                     });
-                });
+                };
 
                 //iOS13+じゃない
             } else {
                 //早くアップデートしてもらうのを祈りながら諦める
                 isGyro = false;
-                gyroIsNotAllowed();
+                // ここに何か関数
+                appendMessage("[Log] 早くアップデートしてもらうのを祈りながら諦める");
             }
+            gyroIsNotAllowed();
         }
     }, 300);
 }
 
 function gyroIsAllowed() {
-    msg = "[OK]ジャイロセンサーが利用可能になりました";
-    const msgTag = document.getElementById("msg");
-    msgTag.innerText = msg;
-    console.log(msg);
+    msg = "[OK]ジャイロセンサーを利用することが可能になりました";
+    appendMessage(msg);
 }
 
 function gyroIsNotAllowed() {
-    msg = "[ERROR]ジャイロセンサーが利用できません";
+    msg = "[ERROR]ジャイロセンサーを利用できません";
+    appendMessage(msg);
+}
+
+function appendMessage(msg) {
     const msgTag = document.getElementById("msg");
-    msgTag.innerText = msg;
+    const newMsgElement = document.createElement("div");
+    newMsgElement.textContent = msg;
+    msgTag.insertBefore(newMsgElement, msgTag.firstChild);
     console.log(msg);
 }
